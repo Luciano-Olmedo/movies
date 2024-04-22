a5737308df5b288ccfd25ce788f6bf20fd659805
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-21 21:55:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='mx-auto text-center items-center bg-slate-100 flex '  >
+        <h4 className='text-3xl'>Hola</h4>
+
+        <div className='mt-6 border-2 border-red-500'>
+          <button className=''>Click</button>
+          <input type="text" />
+
+
+        </div>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default App
