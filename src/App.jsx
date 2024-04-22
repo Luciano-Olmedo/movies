9976eec654f8238dec6587fc04fb4fe9efbcd3b6
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-22 20:53:39 -0300

    home movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+//scale 3af207
 
 const App = () => {
   return (
     <>
-      <div className='mx-auto text-center items-center bg-slate-100 flex '  >
-        <h4 className='text-3xl'>Hola</h4>
-
-        <div className='mt-6 border-2 border-red-500'>
-          <button className=''>Click</button>
-          <input type="text" />
-
-
+      <div className=' grid place-items-center  lg:mt-4'  >
+        <div className='flex '>
+          <h4 className='text-3xl text-center font-bold text-white '>Magic Movies</h4>
         </div>
-
+        <div className='mt-6 flex gap-4'>
+          <button className=' p-4 border-2 border-white rounded-lg text-2xl hover:bg-[#3af207] text-white'>Search Movies</button>
+          <input 
+          type="text" 
+          className='text-center w-96 rounded-lg text-2xl   '
+          placeholder='Enter movie to search' />
+        </div>
       </div>
 
 
