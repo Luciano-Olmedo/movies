627dfe929bfa8fc6ae2eb3ca7b77efe193616092
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-23 19:34:53 -0300

    test cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cards from './cards'
 //scale 3af207
 
 const App = () => {
@@ -10,14 +11,23 @@ const App = () => {
         </div>
         <div className='mt-6 flex gap-4'>
           <button className=' p-4 border-2 border-white rounded-lg text-2xl hover:bg-[#3af207] text-white'>Search Movies</button>
-          <input 
-          type="text" 
-          className='text-center w-96 rounded-lg text-2xl   '
-          placeholder='Enter movie to search' />
+          <input
+            type="text"
+            className='text-center w-96 rounded-lg text-2xl   '
+            placeholder='Enter movie to search' />
         </div>
+      
+      <div className='grid grid-cols-4 gap-10 place-content-center'>
+        <Cards />
+        <Cards />
+        <Cards />
+        <Cards />
+
       </div>
 
 
+
+      </div>
     </>
   )
 }
---
+++ src/Cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import asd from '../src/asd.jpg'
+
+const Cards = () => {
+    return (
+        <div className='mt-12 '>
+            <div className='w-72 h-60'>
+                <img src={asd} alt="agua " />
+
+
+            </div>
+
+        </div>
+    )
+}
+
+
+export default Cards
