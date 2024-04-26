717a1bb10b5fb1eac1d93a0bdb74b35b74c1427f
Luciano-Olmedo <lucianonicolasolmedo@gmail.com>
2024-04-26 20:09:34 -0300

    connecting to api but have some problems with the json files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
-import Cards from './cards'
+import Cards from './Cards'
 //scale 3af207
-// https://api.themoviedb.org/3/movie/550?api_key=738a868f38e105fd1a1fc07338653dc4 example
+
 const App = () => {
-  const urlBase =asd
+  const urlBase = 'api.themoviedb.org/3/movie'
   const API_KEY = '738a868f38e105fd1a1fc07338653dc4'
+
+
   const [busqueda, setBusqueda] = useState('')
-  const [peluculas, setPeliculas] = useState('')
+  const [peliculas, setPeliculas] = useState([])
 
   const handleInputChange = (e) => {
     setBusqueda(e.target.value)
@@ -14,13 +16,12 @@ const App = () => {
   }
   const fetchPeliculas = async () => {
     try {
-
-      const response = await fetch(``)
-
+      const response = await fetch(`${urlBase}?query=${busqueda}&api_key=${API_KEY}`)
+      const data = await response.json()      
+      setPeliculas(data)
     } catch (error) {
       console.error(error)
     }
-
   }
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -33,6 +34,7 @@ const App = () => {
         <div className='flex '>
           <h4 className='text-3xl text-center font-bold text-white '>Magic Movies</h4>
         </div>
+
         <form className='mt-6 flex gap-4' onSubmit={handleSubmit}>
           <button type='submit' className=' p-4 border-2 border-white rounded-lg text-2xl hover:bg-[#3af207] text-white'>Search Movies</button>
           <input
@@ -50,6 +52,7 @@ const App = () => {
           <Cards />
           <Cards />
           <Cards />
+         
 
         </div>
 
